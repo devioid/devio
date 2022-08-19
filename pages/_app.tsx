592c9e1446d65345d8a8805b9265eddd61d0e1d9
welsonoktario <welsonoktario@yahoo.com>
2022-08-19 10:07:13 +0700

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import { Layout } from 'components/layouts/layout'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp
+export default App
