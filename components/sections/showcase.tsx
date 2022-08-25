419ffb1d3e9ac1aa23130ea9e4d59d6156067556
erlanggagatum <erlanggagatum@gmail.com>
2022-08-25 14:01:27 +0700

    add section showcase and responsiveness adjustment in navbar

---
+++ components/sections/showcase.tsx
@@ -0,0 +1,40 @@
+
+
+import { SectionContentHalf } from "@components/section-content-half"
+import { SectionHeader } from "@components/section-header"
+import { Badge } from "@components/badge"
+import academic from "@assets/img/academic.jpg"
+import accounting from "@assets/img/accounting.jpg"
+
+export const Showcase = () => {
+  return (
+    <section className="bg-transparent w-full h-auto" data-scroll-section>
+      <SectionHeader
+        title="Showcase"
+        description="Here are some projects that we have working on"/>
+
+      <SectionContentHalf
+        header="Accounting Information System"
+        imageSrc={accounting}
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur ultricies fermentum. Praesent nec vulputate urna. Maecenas dolor libero, commodo et quam quis, ullamcorper auctor risus. "
+        fullscreen={true}
+        reverse={true}
+        >
+          <Badge>Laravel</Badge>
+          <Badge>VueJS</Badge>
+        </SectionContentHalf>
+
+
+      <SectionContentHalf
+        header="Academic Information System"
+        imageSrc={academic}
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur ultricies fermentum. Praesent nec vulputate urna. Maecenas dolor libero, commodo et quam quis, ullamcorper auctor risus. "
+        fullscreen={true}
+        reverse={false}
+        >
+          <Badge>Laravel</Badge>
+          <Badge>VueJS</Badge>
+        </SectionContentHalf>
+    </section>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Home } from '@components/sections/home'
 import { Platforms } from '@components/sections/platforms'
+import { Showcase } from '@components/sections/showcase'
 import { Services } from 'components/sections/services'
 import { WhyUs } from 'components/sections/why-us'
 import type { NextPage } from 'next'
@@ -11,6 +12,7 @@ const Index: NextPage = () => {
       <Services />
       <Platforms />
       <WhyUs />
+      <Showcase />
     </>
   )
 }
