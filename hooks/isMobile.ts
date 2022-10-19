a8ca462bd31d5692fc6e4681a76988bdc2ad6cd0
erlanggagatum <erlanggagatum@gmail.com>
2022-10-19 15:29:55 +0700

    revise use mobile hooks screen

--- hooks/isMobile.ts
+++ hooks/isMobile.ts
@@ -3,5 +3,5 @@ import { useWindowDimensions } from './useWindowDimensions'
 export const useIsMobile = () => {
   const { width } = useWindowDimensions()
 
-  return width && width <= 768
+  return width && width < 768
 }
